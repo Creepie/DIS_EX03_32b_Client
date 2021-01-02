b646ce05162b40c25701f635f16029dd604e9630
MarioEberth <marioeberth@gmail.com>
2021-01-02 15:53:35 +0100

    update javaDoc comments

--- src/eberthClient/ClientA.java
+++ src/eberthClient/ClientA.java
@@ -4,8 +4,15 @@ import eberthClient.generate.EnvData;
 import eberthClient.generate.EnvironmentData;
 import eberthClient.generate.OpenWeatherService;
 
+/**
+ * this class implement the Client with variant A
+ */
 public class ClientA {
 
+    /**
+     * in the main the Client B program starts
+     * @param args is not used
+     */
     public static void main(String[] args) {
         OpenWeatherService srv = new OpenWeatherService();
         EnvironmentData soap = srv.getOpenWeatherPort();
--- src/eberthClient/ClientB.java
+++ src/eberthClient/ClientB.java
@@ -7,17 +7,29 @@ import javax.xml.ws.Service;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+/**
+ * this class implement the Client with variant B
+ */
 public class ClientB {
 
+    /**
+     * mSOAP is to get the Server service
+     * in this case its the OpenWeatherService
+     */
     static EnvironmentData mSOAP;
 
+    /**
+     * in the main the Client B program starts
+     * @param args is not used
+     * @throws MalformedURLException if something goes wrong
+     */
     public static void main(String[] args) throws MalformedURLException {
         Service service = Service.create(
                 new URL("http://localhost:8081/OpenWeather?wsdl"),
                 new QName("http://eberth/",
                         "OpenWeatherService"));
         mSOAP = service.getPort(EnvironmentData.class);
-        
+
         System.out.println(mSOAP.requestEnvironmentDataTypes());
         String[] locations = mSOAP.requestEnvironmentDataTypes().toArray(new String[0]);
 
